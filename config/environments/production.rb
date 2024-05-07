1c62707683704283b72c5057882c4d2ccb8dbb53
Khánh Phùng <91212840+pmk2k1@users.noreply.github.com>
2024-05-08 01:12:49 +0700

    Improvement - Create Api forgor password, change password, send to email - eidt production deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'zens-restaurant.onrender.com', protoco: 'https' }
+  config.action_mailer.default_url_options = { host: 'zens-restaurant.onrender.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
